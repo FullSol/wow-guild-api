1c574aaa087778359ebecc288429d939ddc10efe
FullSol <ceraines@student.fullsail.edu>
2023-09-16 13:54:58 -0400

    profile validation
    - profile validation tests created as non passing tests
    - code written to pass the tests

---
+++ tests/validations/profileVallidation.test.js
@@ -0,0 +1,166 @@
+const {
+  createSchema,
+  updateSchema,
+} = require("../../validations/profileValidations");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe.only("Profile Validations", () => {
+  describe("create schema", () => {
+    it("should validate a valid update Schema object", () => {
+      const validJsonObject = {
+        username: "JamesBond99",
+        password: "password",
+        repeat_password: "password",
+        email: "jb@gmail.com",
+        battleTag: "jb]BTag#1234",
+        discord: "double07",
+        twitch: "double07T",
+        x: "double07X",
+        youtube: "double07Vids",
+        wowhead: "double07WH",
+        primary_language: "English",
+        about: "The real secret agent",
+      };
+      const result = updateSchema.validate(validJsonObject);
+      expect(result.error).to.be.undefined;
+    });
+
+    it("should not validate an invalid empty create Schema object", () => {
+      const invalidData = {
+        username: "",
+        password: "",
+        repeat_password: "",
+        email: "",
+        battleTag: "",
+        discord: "",
+        twitch: "",
+        x: "",
+        youtube: "",
+        wowhead: "",
+        primary_language: "",
+        about: "",
+      };
+
+      const result = updateSchema.validate(invalidData);
+
+      expect(result.error).to.exist;
+      result.error.details.forEach((detail) => {
+        expect(detail.type).to.equal("string.empty");
+      });
+    });
+
+    it("should not validate an invalid type create Schema object", () => {
+      const invalidData = {
+        username: true,
+        password: true,
+        repeat_password: true,
+        email: true,
+        battleTag: true,
+        discord: true,
+        twitch: true,
+        x: true,
+        youtube: true,
+        wowhead: true,
+        primary_language: true,
+        about: true,
+      };
+
+      const result = createSchema.validate(invalidData);
+
+      expect(result.error).to.exist;
+      result.error.details.forEach((detail) => {
+        expect(detail.type).to.equal("string.base");
+      });
+    });
+
+    it("should not validate an invalid required create Schema object", () => {
+      const invalidData = {};
+
+      const result = createSchema.validate(invalidData);
+      expect(result.error).to.exist;
+      result.error.details.forEach((detail) => {
+        expect(detail.type).to.equal("any.required");
+      });
+    });
+  });
+
+  describe("update schema", () => {
+    it("should validate a valid update update object", () => {
+      const validJsonObject = {
+        username: "JamesBond99",
+        password: "password",
+        repeat_password: "password",
+        email: "jb@gmail.com",
+        battleTag: "jb]BTag#1234",
+        discord: "double07",
+        twitch: "double07T",
+        x: "double07X",
+        youtube: "double07Vids",
+        wowhead: "double07WH",
+        primary_language: "English",
+        about: "The real secret agent",
+      };
+      const result = updateSchema.validate(validJsonObject);
+      expect(result.error).to.be.undefined;
+    });
+
+    it("should not validate an invalid empty update Schema object", () => {
+      const invalidData = {
+        username: "",
+        password: "",
+        repeat_password: "",
+        email: "",
+        battleTag: "",
+        discord: "",
+        twitch: "",
+        x: "",
+        youtube: "",
+        wowhead: "",
+        primary_language: "",
+        about: "",
+      };
+
+      const result = updateSchema.validate(invalidData);
+
+      expect(result.error).to.exist;
+      result.error.details.forEach((detail) => {
+        expect(detail.type).to.equal("string.empty");
+      });
+    });
+
+    it("should not validate an invalid type update Schema object", () => {
+      const invalidData = {
+        username: true,
+        password: true,
+        repeat_password: true,
+        email: true,
+        battleTag: true,
+        discord: true,
+        twitch: true,
+        x: true,
+        youtube: true,
+        wowhead: true,
+        primary_language: true,
+        about: true,
+      };
+
+      const result = createSchema.validate(invalidData);
+
+      expect(result.error).to.exist;
+      result.error.details.forEach((detail) => {
+        expect(detail.type).to.equal("string.base");
+      });
+    });
+
+    it("should not validate an invalid required update Schema object", () => {
+      const invalidData = {};
+
+      const result = createSchema.validate(invalidData);
+      expect(result.error).to.exist;
+      result.error.details.forEach((detail) => {
+        expect(detail.type).to.equal("any.required");
+      });
+    });
+  });
+});
---
+++ validations/profileValidations.js
@@ -0,0 +1,120 @@
+const Joi = require("joi");
+
+const createSchema = Joi.object({
+  username: Joi.string()
+    .required()
+    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+    .messages({
+      "string.empty": `username cannot be a empty field`,
+      "any.required": `username is a required field`,
+      "string.base": `username should be a type of 'text'`,
+    }),
+  password: Joi.string().required().messages({
+    "string.empty": `password cannot be a empty field`,
+    "any.required": `password is a required field`,
+    "string.base": `password should be a type of 'text'`,
+  }),
+  repeat_password: Joi.valid(Joi.ref("password")).required(),
+  email: Joi.string()
+    .required()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .messages({
+      "string.empty": `email cannot be a empty field`,
+      "any.required": `email is a required field`,
+      "string.base": `email should be a type of 'text'`,
+    }),
+  battleTag: Joi.string().messages({
+    "string.empty": `battleTag cannot be a empty field`,
+    "string.base": `battleTag should be a type of 'text'`,
+  }),
+  discord: Joi.string().messages({
+    "string.empty": `discord cannot be a empty field`,
+    "string.base": `discord should be a type of 'text'`,
+  }),
+  twitch: Joi.string().messages({
+    "string.empty": `twitch cannot be a empty field`,
+    "string.base": `twitch should be a type of 'text'`,
+  }),
+  x: Joi.string().messages({
+    "string.empty": `twitter cannot be a empty field`,
+    "string.base": `twitter should be a type of 'text'`,
+  }),
+  youtube: Joi.string().messages({
+    "string.empty": `youtube cannot be a empty field`,
+    "string.base": `youtube should be a type of 'text'`,
+  }),
+  wowhead: Joi.string().messages({
+    "string.empty": `wowhead cannot be a empty field`,
+    "string.base": `wowhead should be a type of 'text'`,
+  }),
+  primary_language: Joi.string().messages({
+    "string.empty": `primary_language cannot be a empty field`,
+    "string.base": `primary_language should be a type of 'text'`,
+  }),
+  about: Joi.string().messages({
+    "string.empty": `about cannot be a empty field`,
+    "string.base": `about should be a type of 'text'`,
+  }),
+}).options({ abortEarly: false });
+
+const updateSchema = Joi.object({
+  username: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).messages({
+    "string.empty": `username cannot be a empty field`,
+    "any.required": `username is a required field`,
+    "string.base": `username should be a type of 'text'`,
+  }),
+  password: Joi.string().messages({
+    "string.empty": `password cannot be a empty field`,
+    "string.base": `password should be a type of 'text'`,
+  }),
+  repeat_password: Joi.ref("password"),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .messages({
+      "string.empty": `email cannot be a empty field`,
+      "any.required": `email is a required field`,
+      "string.base": `email should be a type of 'email'`,
+    }),
+  battleTag: Joi.string().messages({
+    "string.empty": `battleTag cannot be a empty field`,
+    "any.required": `battleTag is a required field`,
+    "string.base": `battleTag should be a type of 'text'`,
+  }),
+  discord: Joi.string().messages({
+    "string.empty": `discord cannot be a empty field`,
+    "any.required": `discord is a required field`,
+    "string.base": `discord should be a type of 'text'`,
+  }),
+  twitch: Joi.string().messages({
+    "string.empty": `twitch cannot be a empty field`,
+    "string.base": `twitch should be a type of 'text'`,
+  }),
+  x: Joi.string().messages({
+    "string.empty": `twitter cannot be a empty field`,
+    "string.base": `twitter should be a type of 'text'`,
+  }),
+  youtube: Joi.string().messages({
+    "string.empty": `youtube cannot be a empty field`,
+    "string.base": `youtube should be a type of 'text'`,
+  }),
+  wowhead: Joi.string().messages({
+    "string.empty": `wowhead cannot be a empty field`,
+    "string.base": `wowhead should be a type of 'text'`,
+  }),
+  primary_language: Joi.string().messages({
+    "string.empty": `primary_language cannot be a empty field`,
+    "string.base": `primary_language should be a type of 'text'`,
+  }),
+  about: Joi.string().messages({
+    "string.empty": `about cannot be a empty field`,
+    "string.base": `about should be a type of 'text'`,
+  }),
+})
+  .options({ abortEarly: false })
+  .with("username", "password")
+  .with("password", "repeat_password");
+
+module.exports = {
+  createSchema,
+  updateSchema,
+};
