81f5213eb812723fa00ea6bb4c92b92776be77ad
FullSol <ceraines@student.fullsail.edu>
2023-09-18 13:15:55 -0400

    Update profileService.test.js
    created tests for the update method

--- tests/services/profileService.test.js
+++ tests/services/profileService.test.js
@@ -1,8 +1,6 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 const Service = require("../../services/profileService");
-const { AggregateError, json } = require("sequelize");
-const { invalid } = require("joi");
 
 describe.only("Profile Service", () => {
   const jsonObjectArray = [
@@ -190,11 +188,10 @@ describe.only("Profile Service", () => {
 
     it("should handle missing id request", async () => {
       // Arrange
+      const validData = { id: 1, field1: "data1" };
       const invalidId = null; // This should cause an error
       const mockRepo = {
-        findByPk: sinon
-          .stub()
-          .throws(new Error("Resource ID must be numerical")),
+        findByPk: sinon.stub().resolves(),
       };
       const service = new Service(mockRepo);
 
@@ -230,6 +227,121 @@ describe.only("Profile Service", () => {
       } catch (error) {
         // Assert
         expect(mockRepo.findByPk.called).true;
+        expect(error.constructor.name).to.equal("Error");
+        expect(error.message).to.equal(`Internal Server Error`);
+      }
+    });
+  });
+
+  describe("Update", () => {
+    it("should return message: resource updated for a valid request", async () => {
+      // Arrange
+      const id = 1;
+      const mockRepo = {
+        update: sinon.stub().resolves(1),
+      };
+      const updateSchema = { validate: sinon.stub().returnsThis() };
+      const validJsonObject = { field1: "data1" };
+      const service = new Service(mockRepo);
+      service.setUpdateSchema(updateSchema);
+
+      // Act
+      const result = await service.update(id, validJsonObject);
+
+      // Assert
+      expect(updateSchema.validate.called).to.be.true;
+      expect(mockRepo.update.called).to.be.true;
+      expect(result).to.equal(1);
+    });
+
+    it("should handle missing id request", async () => {
+      // Arrange
+      const validJsonObject = { field1: "data1" };
+      const invalidId = null;
+      const mockRepo = {
+        update: sinon.stub().resolves(validJsonObject),
+      };
+      const updateSchema = { validate: sinon.stub().returnsThis() };
+      const service = new Service(mockRepo);
+      service.setUpdateSchema(updateSchema);
+
+      try {
+        // Act
+        await service.update(invalidId);
+
+        // If no error is thrown, fail the test
+        expect.fail("Expected Error, but no error was thrown.");
+      } catch (error) {
+        // Assert
+        expect(error.constructor.name).to.equal("Error");
+        expect(error.message).to.equal("Resource ID must be numerical");
+        expect(updateSchema.validate.called).to.be.false;
+        expect(mockRepo.update.called).to.be.false;
+      }
+    });
+
+    it("should return message: invalid request", async () => {
+      // Arrange
+      const id = 1;
+      const mockRepo = {
+        update: sinon.stub().resolves(1),
+      };
+
+      const invalidJsonObject = {}; // bad object
+
+      const validationError = new Error("Validation Error"); // Joi stub setup
+      validationError.details = [
+        {
+          message: '"username" is required',
+          path: ["username"],
+          type: "any.required",
+          context: {
+            key: "username",
+          },
+        },
+      ];
+
+      const updateSchema = { validate: sinon.stub().throws(validationError) };
+      const service = new Service(mockRepo);
+      service.setUpdateSchema(updateSchema);
+
+      try {
+        // Act
+        await service.update(id, invalidJsonObject);
+
+        // If no error is thrown, fail the test
+        expect.fail("Expected ValidationError, but no error was thrown.");
+      } catch (error) {
+        // Assert
+        expect(updateSchema.validate.called).to.be.true;
+        expect(error).to.equal(validationError);
+        expect(error.details).to.deep.equal(validationError.details);
+        expect(mockRepo.update.called).to.be.false;
+      }
+    });
+
+    it("should respond with a 500 Internal service error from the service", async () => {
+      // Arrange
+      const validJsonObject = { field1: "data1" };
+      const id = 1; // valid id
+      const mockRepo = {
+        update: sinon.stub().throws(new Error("Internal Server Error")),
+      };
+
+      const updateSchema = { validate: sinon.stub().returnsThis() };
+      const service = new Service(mockRepo);
+      service.setUpdateSchema(updateSchema);
+
+      try {
+        // Act
+        await service.update(id, validJsonObject);
+
+        // If no error is thrown, fail the test
+        expect.fail("Expected ValidationError, but no error was thrown.");
+      } catch (error) {
+        // Assert
+        expect(updateSchema.validate.called).to.be.true;
+        expect(mockRepo.update.called).to.be.true;
         expect(error.constructor.name).to.equal("Error");
         expect(error.message).to.equal(`Internal Server Error`);
       }
