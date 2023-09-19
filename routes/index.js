a173411036ea855c31d1c0694b74e6919b3408c9
FullSol <ceraines@student.fullsail.edu>
2023-09-19 08:57:19 -0400

    Update index.js
    updated routes to include profile

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 "use strict";
+// models
+const { Profile } = require("../models");
 
 // Index
 const indexController = require("../controllers/index");
 
+// Profiles
+const ProfileService = require("../services/profileService");
+const profileService = new ProfileService(Profile);
+const profileController = require("../controllers/profileController")(
+  profileService
+);
+
 module.exports = (app) => {
   app.use("/", indexController);
   app.use("/api/v1/", indexController);
+  app.use("/api/v1/profiles", profileController);
 };
