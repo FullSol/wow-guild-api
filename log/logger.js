dfc2936a443822529f73d74c6db48880300cd870
FullSol <ceraines@student.fullsail.edu>
2023-09-19 00:13:32 -0400

    Update logger.js
    turned off console output

--- log/logger.js
+++ log/logger.js
@@ -4,7 +4,7 @@ const logger = winston.createLogger({
   level: "info", // You can set the desired log level
   format: winston.format.json(),
   transports: [
-    new winston.transports.Console(), // Output to console
+    // new winston.transports.Console(), // Output to console
     new winston.transports.File({ filename: "error.log", level: "error" }), // Log errors to a file
     new winston.transports.File({ filename: "combined.log" }), // Log all levels to another file
   ],
