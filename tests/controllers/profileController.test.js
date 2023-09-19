a09157bda9605f395fb403384713e9c8d56fcdb6
FullSol <ceraines@student.fullsail.edu>
2023-09-19 00:48:05 -0400

    Update profileController.test.js
    created tests for update method

--- tests/controllers/profileController.test.js
+++ tests/controllers/profileController.test.js
@@ -233,22 +233,129 @@ describe.only("Profile Controller", () => {
       // Arrange
       const jsonObject = [];
 
-      mockService.readAll.resolves(jsonObject);
+      mockService.readOne.resolves(jsonObject);
 
       app.use("/api/v1/profiles", Controller(mockService));
 
       // Act
       const response = await supertest(app)
-        .get("/api/v1/profiles/")
+        .get("/api/v1/profiles/1")
         .send(jsonObject)
         .set("Accept", "application/json")
         .then((response) => response)
         .catch((error) => error);
 
       // Assert
-      expect(mockService.readAll.calledOnce).to.be.true;
+      expect(mockService.readOne.calledOnce).to.be.true;
       expect(response.status).to.equal(200);
       expect(response.body).to.deep.equal(jsonObject);
+    });
+
+    it("Should respond with 500 internal server error from service", async () => {
+      // Arrange
+      const jsonObject = {
+        field1: "data1",
+      };
+
+      mockService.readOne.throws(new Error("Internal Server Error"));
+
+      app.use("/api/v1/profiles/", Controller(mockService));
+
+      // Act
+      const response = await supertest(app)
+        .get("/api/v1/profiles/1")
+        .send(jsonObject)
+        .set("Accept", "application/json")
+        .then((response) => response)
+        .catch((error) => error);
+
+      // Assert
+      expect(mockService.readOne.calledOnce).to.be.true;
+      expect(response.status).to.equal(500);
+      expect(response.body).to.deep.equal({
+        status: "error",
+        message: "Internal Server Error",
+      });
+    });
+  });
+
+  describe("PATCH /api/v1/profiles/:id", () => {
+    it("Should respond with an object based on id", async () => {
+      // Arrange
+      const jsonObject = {
+        field1: "data1",
+      };
+      mockService.update.resolves(1);
+
+      app.use("/api/v1/profiles/", Controller(mockService));
+
+      // Act
+      const response = await supertest(app)
+        .patch("/api/v1/profiles/1")
+        .send(jsonObject)
+        .set("Accept", "application/json")
+        .then((response) => response)
+        .catch((error) => error);
+
+      // Assert
+      expect(mockService.update.calledOnce).to.be.true;
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(1);
+    });
+
+    it("should respond with 400 ValidationError", async () => {
+      // Arrange
+      const invalidJsonObject = {}; // bad object
+      const errors = [
+        {
+          message: '"username" is required',
+          path: ["username"],
+          type: "any.required",
+          context: {
+            key: "username",
+          },
+        },
+      ];
+      const message = "Validation Error";
+
+      mockService.update.throws(new AggregateValidationError(errors, message));
+
+      app.use("/api/v1/profiles", Controller(mockService));
+
+      const response = await supertest(app)
+        .patch("/api/v1/profiles/1")
+        .send(invalidJsonObject)
+        .then((response) => response)
+        .catch((error) => error);
+
+      // Assert
+      expect(mockService.update.calledOnce).to.be.true;
+      expect(response.status).to.equal(400);
+      expect(response.body.status).to.equal("error");
+      expect(response.body.message).to.equal("Validation failed");
+      expect(response.body.data.errors).to.be.an("array").that.is.not.empty;
+    });
+
+    it("should respond with internal server error due to invalid id", async () => {
+      // Arrange
+      const jsonObject = {
+        field1: "data1",
+      };
+      mockService.update.resolves(jsonObject);
+
+      app.use("/api/v1/profiles/", Controller(mockService));
+
+      // Act
+      const response = await supertest(app)
+        .patch("/api/v1/profiles/doggie")
+        .send(jsonObject)
+        .set("Accept", "application/json")
+        .then((response) => response)
+        .catch((error) => error);
+
+      // Assert
+      expect(mockService.update.calledOnce).to.be.false;
+      expect(response.status).to.equal(500);
     });
 
     it("Should respond with 500 internal server error from service", async () => {
