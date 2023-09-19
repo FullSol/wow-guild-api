7910d05c6f677c7fca900fe6033192493a31659e
FullSol <ceraines@student.fullsail.edu>
2023-09-19 08:52:48 -0400

    Revert "8 bug failing tests"

--- hooks/mochaHooks.js
+++
@@ -1,7 +0,0 @@
-const sinon = require("sinon");
-
-exports.mochaHooks = {
-  afterEach() {
-    if (sinon.restore) sinon.reset();
-  },
-};
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// Index
-const indexController = require("../controllers/index");
-
-module.exports = (app) => {
-  app.use("/", indexController);
-  app.use("/api/v1/", indexController);
-};
