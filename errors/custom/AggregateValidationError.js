de651b2b1f32a743422fee6fb121c2a12a6df17b
FullSol <ceraines@student.fullsail.edu>
2023-09-21 11:51:03 -0400

    Revert "16 bug undesired controller endpoint response"

--- errors/custom/AggregateValidationError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class AggregateValidationError extends AggregateError {
-  constructor(arg1, arg2) {
-    super(arg1, arg2);
-    this.name = "ValidationError";
-  }
-}
-exports.AggregateValidationError = AggregateValidationError;
--- errors/custom/SequelizeUniqueConstraintError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class SequelizeUniqueConstraintError extends AggregateError {
-  constructor(arg1, arg2) {
-    super(arg1, arg2);
-    this.name = "SequelizeUniqueConstraintError";
-  }
-}
-exports.SequelizeUniqueConstraintError = SequelizeUniqueConstraintError;
