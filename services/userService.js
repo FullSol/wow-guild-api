86bc2fc0814b2b976dbc275bc415cf1bfdb9ff93
FullSol <ceraines@student.fullsail.edu>
2023-09-24 09:43:54 -0400

    Update userService.js
    - added to logging message

--- services/userService.js
+++ services/userService.js
@@ -65,7 +65,10 @@ class UserService {
       // Compare the provided password with the stored hash
       const isMatch = await bcrypt.compare(password, storedUser.password);
 
-      if (isMatch) return storedUser; // Accept authentication
+      if (isMatch) {
+        logger.info(`${storedUser.username} accessed.`);
+        return storedUser;
+      } // Accept authentication
       else
         throw new AuthenticationFailureError(
           "Username or Password not recognized"
