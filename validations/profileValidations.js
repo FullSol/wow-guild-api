1c1697564094b8cf6e53976d4c6adf90c033410d
FullSol <ceraines@student.fullsail.edu>
2023-09-26 08:57:01 -0400

    Update profileValidations.js
    - added RegEx pattern for batle_net

--- validations/profileValidations.js
+++ validations/profileValidations.js
@@ -1,12 +1,14 @@
 const Joi = require("joi");
 
 const updateSchema = Joi.object({
-  battle_net: Joi.string().pattern(new RegExp("")).messages({
-    "string.empty": `battle_net cannot be empty.`,
-    "any.required": `battle_net is a required field.`,
-    "string.base": `battle_net should be a type of 'text'.`,
-    "string.pattern.base": `battle_net should follow the blizzard required pattern`,
-  }),
+  battle_net: Joi.string()
+    .pattern(new RegExp("^[A-Za-z0-9]+#[0-9]+$"))
+    .messages({
+      "string.empty": `battle_net cannot be empty.`,
+      "any.required": `battle_net is a required field.`,
+      "string.base": `battle_net should be a type of 'text'.`,
+      "string.pattern.base": `battle_net should follow the blizzard required pattern`,
+    }),
 }).options({ abortEarly: false });
 
 module.exports = {
