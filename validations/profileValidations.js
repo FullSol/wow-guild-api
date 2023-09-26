ad557c96a9cbeb76f1f3a53833e5a1ac75c4a051
FullSol <ceraines@student.fullsail.edu>
2023-09-26 10:24:48 -0400

    Update profileValidations.js

--- validations/profileValidations.js
+++ validations/profileValidations.js
@@ -1,11 +1,15 @@
 const Joi = require("joi");
 
 const updateSchema = Joi.object({
-  battle_net: Joi.string()
+  userId: Joi.string().guid({ version: "uuidv4" }).required().messages({
+    "string.empty": `UUID cannot be empty.`,
+    "any.required": `UUID is a required field.`,
+    "string.guid": `UUID must be in UUIDv4 format.`,
+  }),
+  battleNet: Joi.string()
     .pattern(new RegExp("^[A-Za-z0-9]+#[0-9]+$"))
     .messages({
       "string.empty": `battle_net cannot be empty.`,
-      "any.required": `battle_net is a required field.`,
       "string.base": `battle_net should be a type of 'text'.`,
       "string.pattern.base": `battle_net should follow the blizzard required pattern`,
     }),
