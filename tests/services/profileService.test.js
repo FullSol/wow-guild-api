4024da4333c2d4da4fc4cee65d43fd232aeeab29
FullSol <ceraines@student.fullsail.edu>
2023-09-26 10:25:16 -0400

    Update profileService.test.js
    - modified for the new validation requirements

--- tests/services/profileService.test.js
+++ tests/services/profileService.test.js
@@ -9,7 +9,7 @@ describe("Profile Service", () => {
 
   const profileObject = {
     id: 1,
-    user_id: "some-uuid-string",
+    user_id: "550e8400-e29b-41d4-a716-446655440000",
     battle_net: "user-battle-net",
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -18,22 +18,22 @@ describe("Profile Service", () => {
   describe("Read One", () => {
     it("should return profile of the provided user", async () => {
       // Arrange
-      const mockRepo = { findByUserPk: sinon.stub().resolves(profileObject) };
-      const userId = "some-uuid-string";
+      const mockRepo = { findAll: sinon.stub().resolves(profileObject) };
+      const userId = "550e8400-e29b-41d4-a716-446655440000";
       service = new ProfileService(mockRepo);
 
       // Act
       const result = await service.readOneByUser(userId);
 
       // Assert
-      expect(mockRepo.findByUserPk.called).to.be.true;
+      expect(mockRepo.findAll.called).to.be.true;
       expect(result).to.equal(profileObject);
     });
 
     it("should handle missing id request", async () => {
       // Arrange
       const nullId = null;
-      const mockRepo = { findByUserPk: sinon.stub().resolves(profileObject) };
+      const mockRepo = { findAll: sinon.stub().resolves(profileObject) };
       service = new ProfileService(mockRepo);
 
       try {
@@ -46,14 +46,14 @@ describe("Profile Service", () => {
         // Assert
         expect(error.constructor.name).to.equal("Error");
         expect(error.message).to.equal("Valid User ID must be provided.");
-        expect(mockRepo.findByUserPk.called).to.be.false;
+        expect(mockRepo.findAll.called).to.be.false;
       }
     });
 
     it("should respond with ResourceNotFoundError", async () => {
       // Arrange
       const userId = 1;
-      const mockRepo = { findByUserPk: sinon.stub().resolves(null) };
+      const mockRepo = { findAll: sinon.stub().resolves(null) };
       service = new ProfileService(mockRepo);
 
       try {
@@ -64,7 +64,7 @@ describe("Profile Service", () => {
         expect.fail("Expected ResourceError, but no error was thrown.");
       } catch (error) {
         // Assert
-        expect(mockRepo.findByUserPk.called).of.be.true;
+        expect(mockRepo.findAll.called).of.be.true;
         expect(error.name).to.equal("ResourceNotFoundError");
       }
     });
@@ -73,19 +73,19 @@ describe("Profile Service", () => {
       // Arrange
       const userId = 1;
       const mockRepo = {
-        findByUserPk: sinon.stub().throws(new Error("Internal Server Error")),
+        findAll: sinon.stub().throws(new Error("Internal Server Error")),
       };
       service = new ProfileService(mockRepo);
 
       try {
         // Act
-        await service.readOneByUser;
+        await service.readOneByUser(userId);
 
         // If no error is thrown, fail the test
         expect.fail("Expected Error, but no error was thrown.");
       } catch (error) {
         // Assert
-        expect(mockRepo.findByUserPk.called).true;
+        expect(mockRepo.findAll.called).true;
         expect(error.constructor.name).to.equal("Error");
         expect(error.message).to.equal(`Internal Server Error`);
       }
@@ -93,53 +93,32 @@ describe("Profile Service", () => {
   });
 
   describe("Update", () => {
-    const validProfile = { field1: "data1" };
+    const validProfile = {
+      userId: "550e8400-e29b-41d4-a716-446655440000",
+      battleNet: "user-battle-net",
+    };
     it("should return message: resource updated for a valid request", async () => {
       // Arrange
-
-      const validUserId = "some-user-uuid";
-      const mockRepo = { update: sinon.stub().resolves(validProfile) };
+      const mockRepo = { update: sinon.stub().resolves(profileObject) };
       const updateSchema = { validate: sinon.stub().returnsThis() };
       service = new ProfileService(mockRepo);
       service.setUpdateSchema(updateSchema);
 
       // Act
-      const result = await service.update(validUserId);
+      const result = await service.update(validProfile);
 
       // If no error is thrown, fail the test
       expect(updateSchema.validate.called).to.be.true;
       expect(mockRepo.update.called).to.be.true;
-      expect(result).to.deep.equal(validProfile);
-    });
-
-    it("should handle missing id request", async () => {
-      // Arrange
-      const validProfile = { field1: "data1" };
-      const invalidId = null;
-      const mockRepo = { update: sinon.stub().resolves(validProfile) };
-      const updateSchema = { validate: sinon.stub().returnsThis() };
-      service = new ProfileService(mockRepo);
-      service.setUpdateSchema(updateSchema);
-
-      try {
-        // Act
-        await service.update(invalidId);
-
-        // If no error is thrown, fail the test
-        expect.fail("Expected Error, but no error was thrown.");
-      } catch (error) {
-        // Assert
-        expect(error.constructor.name).to.equal("Error");
-        expect(error.message).to.equal("Resource ID must be numerical");
-        expect(updateSchema.validate.called).to.be.false;
-        expect(mockRepo.update.called).to.be.false;
-      }
+      expect(result).to.deep.equal(profileObject);
     });
 
     it("should respond with ResourceNotFoundError", async () => {
       // Arrange
-      const id = 1;
-      const validData = { field1: "data1" };
+      const validProfile = {
+        userId: "550e8400-e29b-41d4-a716-446655440000",
+        battleNet: "user-battle-net",
+      };
       const mockRepo = { update: sinon.stub().resolves({ 0: 0 }) };
       const updateSchema = { validate: sinon.stub().returnsThis() };
       service = new ProfileService(mockRepo);
@@ -147,10 +126,10 @@ describe("Profile Service", () => {
 
       try {
         // Act
-        const result = await service.update(id, validData);
+        await service.update(validProfile);
 
         // Assert
-        expect(result).to.deep.equal(null);
+        expect.fail("Expected ResourceNotFound error, but received no error.");
       } catch (error) {
         // Assert
         expect(mockRepo.update.called).to.be.true;
