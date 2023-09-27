7714961ea8a8cdbcebeee6f80470aa3d67d72b99
FullSol <ceraines@student.fullsail.edu>
2023-09-27 10:11:33 -0400

    Update app.js
    mounted profile controller to the application

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const limiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per windowMs
 });
 
+// Import Routes
+const profileRoutes = require("./routes/profileRoutes");
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -40,7 +43,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", limiter); // Apply to all routes under /api
 
+// Mount the routes onto the app
 routes(app);
+app.use("/api/v1/profiles", profileRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
