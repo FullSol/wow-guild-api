361d102bdd6827537ecb9620b02542dd3b7c722f
FullSol <ceraines@student.fullsail.edu>
2023-09-27 10:08:17 -0400

    Update index.js
    - fixed import

--- services/index.js
+++ services/index.js
@@ -1,4 +1,4 @@
 const UserService = require("./userService");
-const ProfileService = require("./ProfileService");
+const ProfileService = require("./profileService");
 
 module.exports = { UserService, ProfileService };
