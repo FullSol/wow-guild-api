1c52fcf57d21efd0ca4782cf782ae59aa4064ed9
FullSol <ceraines@student.fullsail.edu>
2023-09-27 10:07:04 -0400

    Update index.js
    - added profile imports/exports

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const indexController = require("./indexController");
 const userController = require("./userController");
+const profileController = require("./profileController");
 
 module.exports = (service) => {
   return {
     indexController: indexController(service),
     userController: userController(service),
+    profileController: profileController(service),
   };
 };
