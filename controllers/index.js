dabd2f0e35a232165371428347adaed7b2424c8d
FullSol <ceraines@student.fullsail.edu>
2023-09-28 10:12:05 -0400

    Update index.js
    - removed indexController moved functionality to the userControler

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
-const indexController = require("./indexController");
 const userController = require("./userController");
 const profileController = require("./profileController");
 
 module.exports = (service) => {
   return {
-    indexController: indexController(service),
     userController: userController(service),
     profileController: profileController(service),
   };
