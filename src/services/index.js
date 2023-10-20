b580811c5f3388c3d6bc699fd3d4a0529c20b571
FullSol <ceraines@student.fullsail.edu>
2023-10-20 08:15:12 -0400

    added starter files for autometic updating of bnet

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,11 @@
 const UserService = require("./userService");
 const ProfileService = require("./profileService");
 const CharacterService = require("./characterService");
+const BnetUpdateService = require("./bnetUpdateService");
 
-module.exports = { UserService, ProfileService, CharacterService };
+module.exports = {
+  UserService,
+  ProfileService,
+  CharacterService,
+  BnetUpdateService,
+};
